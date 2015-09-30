c911f534edaeb7662151cc59949168549fb86968
Chi Doanh Mac <chidoanhmac@chis-macbook-pro.local>
2015-09-30 16:10:25 +0700

    Initial Commit

---
+++ TestHoshiTextField/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestHoshiTextField
+//
+//  Created by Luc Chelsea on 9/30/15.
+//  Copyright © 2015 Luc Chelsea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
